2fe3fd7aa3417cc00bc2b0fe5815870158184ad1
Diego Caceres <dado90caceres@gmail.com>
2025-08-30 18:13:50 -0300

    Copy more images

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,15 +8,25 @@ const joinAUALink =
 const imageSections = {
   "apnea-profundidad": {
     title: "Apnea de Profundidad",
-    images: ["/gallery/deep1.jpg"],
+    images: [
+      "/gallery/deep1.jpg",
+      "/gallery/deep1.jpg",
+      "/gallery/deep1.jpg",
+      "/gallery/deep1.jpg",
+    ],
   },
   "exploracion-submarina": {
     title: "Exploración Submarina",
-    images: ["/gallery/sea1.jpg"],
+    images: [
+      "/gallery/sea1.jpg",
+      "/gallery/sea1.jpg",
+      "/gallery/sea1.jpg",
+      "/gallery/sea1.jpg",
+    ],
   },
   entrenamiento: {
     title: "Entrenamiento",
-    images: ["/gallery/pool1.jpg"],
+    images: ["/gallery/pool1.jpg", "/gallery/pool1.jpg", "/gallery/pool1.jpg"],
   },
 };
 
